32240b727207a25be8a3b19eab07b0199b80452f
BuddhiEash <buddhieash@gmail.com>
2021-11-07 21:19:31 +0800

    Initial commit

---
+++ ConsoleAppDesignPatterns/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAppDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testing Singleton design pattern
+            Console.WriteLine("Singleton One Test!");
+            var obj1 = SingletonOne.CreateSingletonObject;
+            var obj2 = SingletonOne.CreateSingletonObject;
+            //Following console outputs show the same hashcode which means both objects use a single instance.
+            //Simply, we have implemented singleton design pattern here!!
+            Console.WriteLine("Object 1 hash value :"+obj1.GetHashCode());
+            Console.WriteLine("Object 2 hash value :"+obj2.GetHashCode());
+        }
+    }
+}
---
+++ ConsoleAppDesignPatterns/SingletonOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppDesignPatterns
+{
+    class SingletonOne
+    {
+        private static SingletonOne _instance;
+        //Private constructor
+        private SingletonOne()
+        {
+        }
+
+        public static SingletonOne CreateSingletonObject
+        {
+            get 
+            { 
+                if (_instance == null) {
+                    _instance = new SingletonOne();
+                }
+                return _instance;
+            }
+        }
+    }
+}
